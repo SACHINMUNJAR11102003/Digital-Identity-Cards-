da8db53276dd8557aa375c2d28a6b5efeffc0efd
SACHINMUNJAR11102003 <122804877+SACHINMUNJAR11102003@users.noreply.github.com>
2024-09-18 17:51:22 +0530

    Add files via upload

---
+++ ServiceInterface.java
@@ -0,0 +1,9 @@
+package com.example.food_delivery.Food.Delivery.service;
+
+import java.util.List;
+
+import com.example.food_delivery.Food.Delivery.entity.AllAttendance;
+
+public interface ServiceInterface {
+    List<AllAttendance> getAllAttendance();
+}
